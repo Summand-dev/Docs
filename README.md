1c21d34b5379af96723d3e880702a9e80fe3bb78
Alireza Arabi <alirezaaraby5@gmail.com>
2024-04-22 09:59:28 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Summand Docs
+
+This is the documentation repository for (Summand)["https://summand.dev"] project
---
+++ theme.config.jsx
@@ -0,0 +1,12 @@
+export default {
+    docsRepositoryBase: "https://github.com/summand-dev/docs/tree/main",
+    project: {
+      link: 'https://github.com/summand-dev/summand'
+    },
+    logo: <b>Summand Docs</b>,
+    navigation:
+    {
+      prev: true,
+      next: true
+    }
+  }
